38f16d4e36c14e0d6f5838aaa8ddbfded551bf08
JeffKatzy <jeffkatzygit config --global user.name JeffKatzy>
2022-01-31 15:17:45 -0500

    remove solutions

--- backend/api/models/venue.py
+++ backend/api/models/venue.py
@@ -1,8 +1,2 @@
 class Venue:
-    def __init__(self, id, name, longitude, latitude, zip_code, category):
-        self.id = id
-        self.name = name
-        self.longitude = longitude
-        self.latitude = latitude
-        self.zip_code = zip_code
-        self.category = category
+    pass
