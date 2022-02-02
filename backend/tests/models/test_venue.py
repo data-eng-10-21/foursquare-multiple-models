66188c716442beed7b394dcaa1b686c9d47d7d20
JeffKatzy <jeffkatzygit config --global user.name JeffKatzy>
2022-02-02 17:09:51 -0500

    add lesson

--- backend/tests/models/test_venue.py
+++ backend/tests/models/test_venue.py
@@ -1,13 +1,25 @@
 from api.models.venue import Venue
+from api.lib.orm import save
+from api.lib.db import drop_records, drop_tables, test_conn, test_cursor
+import pytest
 
-# Here we test that our venue class is set up properly.  
-# We should have the ability to initialize a Venue instance with the 
-# attributes below, and it should set the attributes of the instance accordingly.
+def test_initialize_with_values_of_foursquare_id_name_rating_likes():
+    venue = Venue(foursquare_id = '5b2932a0f5e9d70039787cf2',
+            name = 'Los Tacos', rating = 5, likes = 20, menu_url = 'foobar.com')
+    assert venue.__dict__ == {'foursquare_id': '5b2932a0f5e9d70039787cf2',
+        'name': 'Los Tacos', 'likes': 20, 'menu_url': 'foobar.com', 'rating': 5}
 
-def test_initialize_with_values_of_id_name_latitude_longitude_category_zip_code():
-    venue = Venue(id = '1234', 
-            name = 'Los Tacos', longitude = -73.98, 
-            latitude = 40.7, zip_code = '11201', category = 'Tacos')
-    assert venue.__dict__ == {'id': '1234', 'name': 'Los Tacos',
-  'longitude': -73.98, 'latitude': 40.7, 'zip_code': '11201',
-  'category': 'Tacos'}
+@pytest.fixture()
+def clean_tables():
+    drop_records(test_cursor, test_conn, 'venues')
+    yield
+    drop_records(test_cursor, test_conn, 'venues')
+
+def test_saves_to_the_db(clean_tables):
+        venue = Venue(foursquare_id = '1245', name = 'Los Tacos',
+        price = 2, rating = 3.5, likes = 100, menu_url = 'www.foobar.com'
+        )
+        save(venue, test_conn, test_cursor)
+        test_cursor.execute('SELECT * FROM venues ORDER BY id DESC LIMIT 1')
+        record = test_cursor.fetchone()
+        assert record[1:3] == ('1245', 'Los Tacos')
